8d231a160d77eb7679b3dbf5adc083bb83762f02
mtsteo <mateusteotonho@gmail.com>
2024-04-24 23:09:29 -0300

    fix: Routes FindBycategory is not necessary

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -21,31 +21,14 @@ export class ProductsService {
       .createQueryBuilder('product')
       .leftJoinAndSelect('product.category_id', 'category');
 
-    // Verifica se o nome da categoria foi fornecido
     if (category) {
-      // Se o nome da categoria foi fornecido, filtra os produtos pela categoria com esse nome
       query = query.where('category.name = :category', { category });
     }
-
-    // Adiciona as cláusulas de limit e offset
     query = query.skip(page).take(limit);
 
-    // Adiciona a ordenação
     query = query.orderBy('product.price', order as any);
 
-    // Retorna os produtos, com ou sem filtro de categoria
     return await query.getMany();
-  }
-
-  async findByCategory(name: string, page: number, limit: number) {
-    const skip = (page - 1) * limit;
-    const result = await this.repository.find({
-      where: { category_id: { name: name } },
-      skip,
-      take: limit,
-    });
-    if (result.length <= 0) throw new NotFoundException('Products Not Found!');
-    return result;
   }
 
   async findOne(id: string) {
